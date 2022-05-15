da921643a5242dcc7b9fa43ccb209970b7de398b
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-15 14:38:40 +0300

    Feat: Import & initialize login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 bootstrap= Bootstrap()
 db= SQLAlchemy()
@@ -26,5 +32,6 @@ def create_app(config_name):
     # initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     
     return app
