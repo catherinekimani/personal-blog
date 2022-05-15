7c50453e1538a6fd5cb7ab12aa58f8ea01846ac9
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-15 16:30:54 +0300

    Feat: Create login function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,12 @@
-from flask import render_template,redirect,url_for
+from crypt import methods
+from flask import render_template,redirect,url_for,request,flash
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from . import auth
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
-
+# register user
 @auth.route('/register', methods = ['GET','POST'])
 def register():
     form = RegistrationForm()
@@ -17,3 +16,23 @@ def register():
         db.session.commit()
         return redirect(url_for('/auth.login'))
     return render_template('auth/register.html', registration_form = form)
+
+# log in user
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+        
+    return render_template('auth/login.html', login_form = login_form)
+
+# log out user
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
