61dd0e1a8585e8197f5b0309b0e6b15f72ccf46c
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-15 18:23:07 +0300

    Feat:Create update profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template,request,url_for,redirect,abort
 from . import main
 from ..models import User
+from .forms import UpdateProfile
+from .. import db
 
 @main.route('/')
 def index():
@@ -13,7 +15,7 @@ def index():
 
 @main.route('/user/<name>')
 def profile(name):
-    user = User.query.filter_by(username = user).first()
+    user = User.query.filter_by(username = name).first()
     
     if user is None:
         abort(404)
@@ -21,3 +23,21 @@ def profile(name):
     return render_template('profile/profile.html', user = user)
     
     
+# update profile
+@main.route('/user/<name>/update', methods = ['GET', 'POST'])
+def update_profile(name):
+    user = User.query.filter_by(username = name).first()
+    if user is None:
+        abort(404)
+        
+    form = UpdateProfile()
+    
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect(url_for('.profile', name = user.username))
+    
+    return render_template('profile/update.html',form = form)
