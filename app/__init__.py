46b96d69c80e02afc518b1485a1681e0f28a364a
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-15 10:27:33 +0300

    Feat: Import SQLAlchemy & Create db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
+db= SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # creating app configuration
     app.config.from_object(config_options[config_name])
@@ -10,5 +15,8 @@ def create_app(config_name):
     # registering blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    # initializing flask extensions
+    db.init_app(app)
     
     return app
