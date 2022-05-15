663e18db68a3337744f977286da1f5908779c992
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-15 20:32:31 +0300

    Feat: Import and initialize flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+mail = Mail()
 photos = UploadSet('photos', IMAGES)
 bootstrap= Bootstrap()
 db= SQLAlchemy()
@@ -35,6 +37,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     
     # configure UploadSet
     configure_uploads(app,photos)
