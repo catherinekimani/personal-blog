5d00e69deee724dd6d99691baa1fdb7f76f54282
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-15 17:42:47 +0300

    Feat: Create profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,request,url_for,redirect,abort
 from . import main
+from ..models import User
 
 @main.route('/')
 def index():
@@ -9,3 +10,14 @@ def index():
     '''
     
     return render_template('index.html')
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = user).first()
+    
+    if user is None:
+        abort(404)
+        
+    return render_template('profile/profile.html', user = user)
+    
+    
