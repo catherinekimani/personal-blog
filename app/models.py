f71d7d876d180537d95710bf0a688142ed062aac
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-16 03:44:35 +0300

    Feat: Create comments class

--- app/models.py
+++ app/models.py
@@ -17,6 +17,8 @@ class User(UserMixin, db.Model):
     bio = db.Column(db.String(255))
     profile = db.Column(db.String())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    blogs = db.relationship('Blog',backref = 'blog',lazy = 'dynamic')
+    comments = db.relationship('Blog',backref = 'blog',lazy = 'dynamic')
     
     @property
     def password(self):
@@ -49,7 +51,7 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title = db.Column(db.String(55))
     category = db.Column(db.String(255))
-    description= db.Column(db.String(255))
+    description= db.Column(db.String(400))
     post_by= db.Column(db.String(255))
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -73,3 +75,23 @@ class Blog(db.Model):
 
     def repr(self):
         return f'Blog {self.description}'
+    
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    text = db.Column(db.Text, nullable=False)
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+    
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_comments(cls,blog_id):
+        comments = Comments.query.filter_by(blog_id=blog_id).all()
+        
+        return comments
+    
+    def __repr__(self):
+        return f'User {self.text}'
