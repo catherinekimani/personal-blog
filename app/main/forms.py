6996e7b83dfd13d09be0bf1887edc081a60605f1
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-16 11:52:06 +0300

    Feat: Create Blog and Comment forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,15 @@ from wtforms.validators import DataRequired
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Add your Bio',validators = [DataRequired()])
     submit = SubmitField('Submit')
+    
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Fashion blog','Fashion blog'),('Travel Blog','Travel Blog'),('Food blog','Food blog')],validators=[DataRequired()])
+    description = TextAreaField('Create your blog', validators=[DataRequired()])
+    submit = SubmitField('Post')
+    
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[DataRequired()])
+    submit = SubmitField('Comment')
